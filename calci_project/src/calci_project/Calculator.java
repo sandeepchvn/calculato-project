570b906e411693e606fbf8c8f0949d6421e00bb3
sandeepchvn <sandeepchvn1999@gmail.com>
2026-03-18 12:00:04 +0530

    master added the project work

---
+++ calci_project/src/calci_project/Calculator.java
@@ -0,0 +1,10 @@
+package calci_project;
+
+public interface Calculator {
+
+	public void add(int a, int b);
+	public void multi(int a, int b);
+	public void div(int a, int b);
+	public void sub(int a, int b);
+	
+}
