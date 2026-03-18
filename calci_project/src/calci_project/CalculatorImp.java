f9d22fd10616e2f3303808296f5b7f4699b40927
sandeepchvn <sandeepchvn1999@gmail.com>
2026-03-18 12:14:43 +0530

    dev1 imp

---
+++ calci_project/src/calci_project/CalculatorImp.java
@@ -0,0 +1,29 @@
+package calci_project;
+
+public class CalculatorImp implements Calculator{
+
+	@Override
+	public void add(int a, int b) {
+		System.out.println(a+b);
+		
+	}
+
+	@Override
+	public void multi(int a, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void div(int a, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sub(int a, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
